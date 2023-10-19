0d13a4d65044c1ba7c33d224e90f893ded6ff4bd
Chris Martin <ch.martin@gmail.com>
2023-10-19 12:40:28 -0600

    add aws-cli

--- main/checks.nix
+++ main/checks.nix
@@ -1,5 +1,6 @@
 { nixpkgs, packages }:
 nixpkgs.stable.lib.lists.foldl' (a: b: a // b) { } [
+  (import ./aws-cli/checks.nix { inherit nixpkgs packages; })
   (import ./fourmolu/checks.nix { inherit nixpkgs packages; })
   (import ./nodejs/checks.nix { inherit nixpkgs packages; })
 ]
