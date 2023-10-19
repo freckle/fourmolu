b4b8654963c9ad38aa756270ec0a4bbf8416b8c6
Chris Martin <ch.martin@gmail.com>
2023-10-19 12:22:50 -0600

    bundle yarn with nodejs

--- main/nodejs/checks.nix
+++ main/nodejs/checks.nix
@@ -4,14 +4,24 @@ let
   inherit (nixpkgs.stable.lib.attrsets) recursiveUpdate;
   inherit (nixpkgs.stable.testers) testEqualContents;
 
-  versionCheck = version: nodejs: testEqualContents {
+  checkNodejsVersion = version: package: testEqualContents {
     assertion = "nodejs is version ${version}";
     expected = writeText "expected" ("v" + version + "\n");
-    actual = runCommand "actual" { nativeBuildInputs = [ nodejs ]; } "node --version > $out";
+    actual = runCommand "actual" { nativeBuildInputs = [ package ]; } "node --version > $out";
+  };
+
+  checkYarnVersion = version: package: testEqualContents {
+    assertion = "yarn is version ${version}";
+    expected = writeText "expected" (version + "\n");
+    actual = runCommand "actual" { nativeBuildInputs = [ package ]; } "yarn --version > $out";
   };
 in
 {
-  nodejs-version-16-20-0 = versionCheck "16.20.0" packages.nodejs-16-20-0;
-  nodejs-version-16-20-1 = versionCheck "16.20.1" packages.nodejs-16-20-1;
-  nodejs-version-16-20-2 = versionCheck "16.20.2" packages.nodejs-16-20-2;
+  nodejs-16-20-0-version = checkNodejsVersion "16.20.0" packages.nodejs-16-20-0;
+  nodejs-16-20-1-version = checkNodejsVersion "16.20.1" packages.nodejs-16-20-1;
+  nodejs-16-20-2-version = checkNodejsVersion "16.20.2" packages.nodejs-16-20-2;
+
+  nodejs-16-20-0-yarn-version = checkYarnVersion "1.22.19" packages.nodejs-16-20-0;
+  nodejs-16-20-1-yarn-version = checkYarnVersion "1.22.19" packages.nodejs-16-20-1;
+  nodejs-16-20-2-yarn-version = checkYarnVersion "1.22.19" packages.nodejs-16-20-2;
 }
--- main/nodejs/default.nix
+++ main/nodejs/default.nix
@@ -1,9 +1,46 @@
 { nixpkgs }:
+let
+  inherit (nixpkgs.stable) symlinkJoin;
+in
+
 rec {
   nodejs-default = nodejs-16-x;
   nodejs-16-x = nodejs-16-20-x;
   nodejs-16-20-x = nodejs-16-20-2;
-  nodejs-16-20-0 = nixpkgs.master-2023-05-06.nodejs_16;
-  nodejs-16-20-1 = nixpkgs.master-2023-07-18.nodejs_16;
-  nodejs-16-20-2 = nixpkgs.stable.nodejs_16;
+
+  nodejs-16-20-0 = (
+    let
+      pkgs = nixpkgs.master-2023-05-06;
+      nodejs = pkgs.nodejs_16;
+      yarn = pkgs.yarn.override { inherit nodejs; };
+    in
+    symlinkJoin {
+      name = "nodejs";
+      paths = [ nodejs yarn ];
+    }
+  );
+
+  nodejs-16-20-1 = (
+    let
+      pkgs = nixpkgs.master-2023-07-18;
+      nodejs = pkgs.nodejs_16;
+      yarn = pkgs.yarn.override { inherit nodejs; };
+    in
+    symlinkJoin {
+      name = "nodejs";
+      paths = [ nodejs yarn ];
+    }
+  );
+
+  nodejs-16-20-2 = (
+    let
+      pkgs = nixpkgs.stable;
+      nodejs = pkgs.nodejs_16;
+      yarn = pkgs.yarn.override { inherit nodejs; };
+    in
+    symlinkJoin {
+      name = "nodejs";
+      paths = [ nodejs yarn ];
+    }
+  );
 }
