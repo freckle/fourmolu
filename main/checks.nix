be7bb4ca0ccffd10298bb32387a7b44f641e562a
Chris Martin <ch.martin@gmail.com>
2023-10-18 20:55:50 -0600

    Add checks, nodejs, fourmolu-0.14, github action (#3)

---
+++ main/checks.nix
@@ -0,0 +1,5 @@
+{ nixpkgs, packages }:
+nixpkgs.stable.lib.lists.foldl' (a: b: a // b) { } [
+  (import ./fourmolu/checks.nix { inherit nixpkgs packages; })
+  (import ./nodejs/checks.nix { inherit nixpkgs packages; })
+]
---
+++ main/fourmolu/checks.nix
@@ -0,0 +1,22 @@
+{ nixpkgs, packages }:
+let
+  inherit (nixpkgs.stable) writeText runCommand;
+  inherit (nixpkgs.stable.lib.attrsets) recursiveUpdate;
+  inherit (nixpkgs.stable.testers) testEqualContents;
+
+  versionCheck = version: fourmolu: testEqualContents {
+    assertion = "fourmolu is version ${version}";
+    expected = writeText "expected" ("fourmolu " + version + "\n");
+    actual = runCommand "actual" { nativeBuildInputs = [ fourmolu ]; } ''
+      fourmolu --version \
+        | head -n1 \
+        | sed -re 's/^(fourmolu [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)\.?.*$/\1/' \
+        > $out
+    '';
+  };
+in
+{
+  fourmolu-0-13-0 = versionCheck "0.13.0" packages.fourmolu-0-13-0;
+  fourmolu-0-13-1 = versionCheck "0.13.1" packages.fourmolu-0-13-1;
+  fourmolu-0-14-0 = versionCheck "0.14.0" packages.fourmolu-0-14-0;
+}
