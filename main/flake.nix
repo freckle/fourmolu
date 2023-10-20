7323a8996ff7644bdfcd6dc582b75c4b32286e83
Chris Martin <ch.martin@gmail.com>
2023-10-20 13:05:31 -0600

    remove unused input

--- main/flake.nix
+++ main/flake.nix
@@ -4,7 +4,6 @@
     nixpkgs-stable-2023-07-25.url = "github:nixos/nixpkgs/6dc93f0daec55ee2f441da385aaf143863e3d671";
     nixpkgs-master-2023-05-06.url = "github:nixos/nixpkgs/16b3b0c53b1ee8936739f8c588544e7fcec3fc60";
     nixpkgs-master-2023-07-18.url = "github:nixos/nixpkgs/08700de174bc6235043cb4263b643b721d936bdb";
-    nixpkgs-master-2023-10-20.url = "github:nixos/nixpkgs/5bf82c5a0be600bf8129bc27977205e0806e5a8b";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs:
