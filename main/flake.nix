d9898bc35ac5730c50fef8b157a311f6262bb3c0
Chris Martin <ch.martin@gmail.com>
2023-10-22 00:36:48 -0600

    add nodejs 18.17.1 (#10)

--- main/flake.nix
+++ main/flake.nix
@@ -4,6 +4,7 @@
     nixpkgs-stable-2023-07-25.url = "github:nixos/nixpkgs/6dc93f0daec55ee2f441da385aaf143863e3d671";
     nixpkgs-master-2023-05-06.url = "github:nixos/nixpkgs/16b3b0c53b1ee8936739f8c588544e7fcec3fc60";
     nixpkgs-master-2023-07-18.url = "github:nixos/nixpkgs/08700de174bc6235043cb4263b643b721d936bdb";
+    nixpkgs-master-2023-09-15.url = "github:nixos/nixpkgs/46688f8eb5cd6f1298d873d4d2b9cf245e09e88e";
     nixpkgs-unstable-2023-10-21.url = "github:nixos/nixpkgs/038b2922be3fc096e1d456f93f7d0f4090628729";
     flake-utils.url = "github:numtide/flake-utils";
   };
--- main/nodejs/v18.nix
+++ main/nodejs/v18.nix
@@ -3,8 +3,23 @@ let
   inherit (import inputs.nixpkgs-stable { inherit system; config = { }; }) symlinkJoin;
 in
 rec {
-  nodejs-18-x = nodejs-18-18-x;
+  # 18.18.0 is not recommended due to https://github.com/webpack-contrib/thread-loader/issues/191
+  nodejs-18-x = nodejs-18-17-x;
+
+  nodejs-18-17-x = nodejs-18-17-1;
   nodejs-18-18-x = nodejs-18-18-0;
+
+  nodejs-18-17-1 = (
+    let
+      nixpkgs = import inputs.nixpkgs-master-2023-09-15 { inherit system; config = { }; };
+      nodejs = nixpkgs.nodejs_18;
+      yarn = nixpkgs.yarn.override { inherit nodejs; };
+    in
+    symlinkJoin {
+      name = "nodejs";
+      paths = [ nodejs yarn ];
+    }
+  );
 
   nodejs-18-18-0 = (
     let
