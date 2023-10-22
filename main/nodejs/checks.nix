4ebb064b9dab5b7382076e7eb13650c51611558c
Chris Martin <chris.martin@renaissance.com>
2023-10-22 00:32:05 -0600

    add nodejs 18.17.1

--- main/nodejs/checks.nix
+++ main/nodejs/checks.nix
@@ -21,10 +21,12 @@ in
   nodejs-16-20-0-version = checkNodejsVersion "16.20.0" packages.nodejs-16-20-0;
   nodejs-16-20-1-version = checkNodejsVersion "16.20.1" packages.nodejs-16-20-1;
   nodejs-16-20-2-version = checkNodejsVersion "16.20.2" packages.nodejs-16-20-2;
+  nodejs-18-17-1-version = checkNodejsVersion "18.17.1" packages.nodejs-18-17-1;
   nodejs-18-18-0-version = checkNodejsVersion "18.18.0" packages.nodejs-18-18-0;
 
   nodejs-16-20-0-yarn-version = checkYarnVersion "1.22.19" packages.nodejs-16-20-0;
   nodejs-16-20-1-yarn-version = checkYarnVersion "1.22.19" packages.nodejs-16-20-1;
   nodejs-16-20-2-yarn-version = checkYarnVersion "1.22.19" packages.nodejs-16-20-2;
+  nodejs-18-17-1-yarn-version = checkYarnVersion "1.22.19" packages.nodejs-18-17-1;
   nodejs-18-18-0-yarn-version = checkYarnVersion "1.22.19" packages.nodejs-18-18-0;
 }
