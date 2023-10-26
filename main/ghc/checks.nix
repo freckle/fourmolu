f27fdf71f228a4164b47931d8ce13afb9fd68e7d
Chris Martin <ch.martin@gmail.com>
2023-10-26 17:27:20 -0600

    add ghc 9.2.7 and 9.2.8, bundled with weeder

---
+++ main/ghc/checks.nix
@@ -0,0 +1,30 @@
+{ inputs, system, packages, ... }:
+let
+  nixpkgs = import inputs.nixpkgs-stable { inherit system; config = { }; };
+  inherit (nixpkgs) writeText runCommand;
+  inherit (nixpkgs.lib.attrsets) recursiveUpdate;
+  inherit (nixpkgs.testers) testEqualContents;
+
+  ghcVersionCheck = version: package: testEqualContents {
+    assertion = "ghc is version ${version}";
+    expected = writeText "expected" ("The Glorious Glasgow Haskell Compilation System, version " + version + "\n");
+    actual = runCommand "actual" { nativeBuildInputs = [ package ]; } ''
+      ghc --version > $out
+    '';
+  };
+
+  weederVersionCheck = version: package: testEqualContents {
+    assertion = "ghc is version ${version}";
+    expected = writeText "expected" ("weeder version " + version + "\n");
+    actual = runCommand "actual" { nativeBuildInputs = [ package ]; } ''
+      weeder --version | head -n1 > $out
+    '';
+  };
+in
+{
+  ghc-9-2-7 = ghcVersionCheck "9.2.7" packages.ghc-9-2-7;
+  ghc-9-2-8 = ghcVersionCheck "9.2.8" packages.ghc-9-2-8;
+
+  weeder-for-ghc-9-2-7 = weederVersionCheck "2.4.1" packages.ghc-9-2-7;
+  weeder-for-ghc-9-2-8 = weederVersionCheck "2.4.1" packages.ghc-9-2-8;
+}
