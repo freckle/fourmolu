04220f8bc1a905659b55319af958f99dc6a05c0e
Chris Martin <ch.martin@gmail.com>
2024-01-17 19:02:41 -0700

    add nixos module

---
+++ main/nixos-modules.nix
@@ -0,0 +1,31 @@
+{ inputs }:
+{
+  docker-for-local-dev =
+    { system, ... }:
+    let
+      pkgs = import inputs.nixos-22.11 { inherit system; };
+    in
+    {
+      # Enable Docker
+      virtualisation.docker.enable = true;
+
+      # Allow Docker to see the host
+      networking.firewall.allowedTCPPorts = [ 3000 ];
+
+      # Pretend that localhost.com is a domain that points to our own machine
+      networking.extraHosts = ''
+        127.0.0.1 localhost.com
+        127.0.0.1 api.localhost.com
+        127.0.0.1 assets.localhost.com
+        127.0.0.1 classroom.localhost.com
+        127.0.0.1 school.localhost.com
+        127.0.0.1 student.localhost.com
+        127.0.0.1 console.localhost.com
+        127.0.0.1 faktory.localhost.com
+        127.0.0.1 tts.localhost.com
+        127.0.0.1 sso.localhost.com
+      '';
+
+      virtualisation.docker.package = pkgs.docker;
+    };
+}
