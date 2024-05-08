39bb837fe2b709cf57c7579cffe01df02899e806
Chris Martin <ch.martin@gmail.com>
2024-05-08 12:05:41 -0600

    enable cachix (#26)
    * enable cachix
    * add ghc 9.6.4, 9.6.5
    * nix flake upgrade

--- main/flake.nix
+++ main/flake.nix
@@ -4,6 +4,7 @@
     nixpkgs-22-11.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs-23-05.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-stable-2023-07-25.url = "github:nixos/nixpkgs/6dc93f0daec55ee2f441da385aaf143863e3d671";
+    nixpkgs-master-2024-05-06.url = "github:nixos/nixpkgs/72a42d2732fa772d831fcc0d15d952b7e0eca80e";
     nixpkgs-master-2023-05-06.url = "github:nixos/nixpkgs/16b3b0c53b1ee8936739f8c588544e7fcec3fc60";
     nixpkgs-master-2023-07-18.url = "github:nixos/nixpkgs/08700de174bc6235043cb4263b643b721d936bdb";
     nixpkgs-master-2023-09-15.url = "github:nixos/nixpkgs/46688f8eb5cd6f1298d873d4d2b9cf245e09e88e";
@@ -15,11 +16,31 @@
   outputs = inputs:
     (inputs.flake-utils.lib.eachDefaultSystem (system:
       rec {
-        packages = import ./packages.nix { inherit inputs system; };
+        packages = import ./packages.nix { inherit inputs system; } //
+        {
+          test1 = lib.haskellBundle
+            {
+              ghcVersion = "ghc-9-6-4";
+              enableHLS = true;
+            };
+          test2 = lib.haskellBundle
+            {
+              ghcVersion = "ghc-9-6-5";
+              enableHLS = true;
+            };
+        };
         lib = import ./lib.nix { inherit inputs system packages; };
         checks = import ./checks.nix { inherit inputs system packages lib; };
       })
     ) // {
       nixosModules = import ./nixos-modules.nix { inherit inputs; };
     };
+  nixConfig = {
+    extra-substituters = [
+      "https://freckle-flakes.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "freckle-flakes.cachix.org-1:d+zszsfs+gamqZPjsGQPtDvpDNhE6pLSmtZDQHYTUDo="
+    ];
+  };
 }
--- main/ghc/default-versions-function.nix
+++ main/ghc/default-versions-function.nix
@@ -1,6 +1,6 @@
 x: x // (with x; rec {
   ghc-default = ghc-9-6-x;
   ghc-9-2-x = ghc-9-2-8;
-  ghc-9-4-x = ghc-9-4-6;
-  ghc-9-6-x = ghc-9-6-3;
+  ghc-9-4-x = ghc-9-4-8;
+  ghc-9-6-x = ghc-9-6-5;
 })
