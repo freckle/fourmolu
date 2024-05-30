299e92478c01d5d5b9895aac540a9666516367e8
Chris Martin <chris.martin@renaissance.com>
2024-05-30 16:48:53 -0600

    add nodejs 18.20.2

--- main/nodejs/v18.nix
+++ main/nodejs/v18.nix
@@ -3,11 +3,11 @@ let
   inherit (import inputs.nixpkgs-stable { inherit system; config = { }; }) symlinkJoin runCommand;
 in
 rec {
-  # 18.18.0 is not recommended due to https://github.com/webpack-contrib/thread-loader/issues/191
-  nodejs-18-x = nodejs-18-17-x;
+  nodejs-18-x = nodejs-18-20-x;
 
   nodejs-18-17-x = nodejs-18-17-1;
   nodejs-18-18-x = nodejs-18-18-0;
+  nodejs-18-20-x = nodejs-18-20-2;
 
   nodejs-18-17-1 = (
     let
@@ -30,6 +30,24 @@ rec {
   nodejs-18-18-0 = (
     let
       nixpkgs = import inputs.nixpkgs-unstable-2023-10-21 { inherit system; config = { }; };
+      nodejs = nixpkgs.nodejs_18;
+      yarn = nixpkgs.yarn.override { inherit nodejs; };
+      pnpm = nixpkgs.nodePackages.pnpm.override { inherit nodejs; };
+      pnpm-bin = runCommand "pnpm" { } ''
+        mkdir -p "$out/bin"
+        ln -s "${pnpm}/lib/node_modules/.bin/pnpm" "$out/bin/pnpm"
+        ln -s "${pnpm}/lib/node_modules/.bin/pnpx" "$out/bin/pnpx"
+      '';
+    in
+    symlinkJoin {
+      name = "nodejs";
+      paths = [ nodejs pnpm pnpm-bin yarn ];
+    }
+  );
+
+  nodejs-18-20-2 = (
+    let
+      nixpkgs = import inputs.nixpkgs-unstable-2024-05-30 { inherit system; config = { }; };
       nodejs = nixpkgs.nodejs_18;
       yarn = nixpkgs.yarn.override { inherit nodejs; };
       pnpm = nixpkgs.nodePackages.pnpm.override { inherit nodejs; };
