5edcf11de9c5140fff4283013561c98a95ef6980
Chris Martin <chris.martin@renaissance.com>
2024-10-20 14:42:28 -0600

    enable HLS on ghc 9.10
    Partially addresses #38

--- main/flake.nix
+++ main/flake.nix
@@ -10,6 +10,7 @@
     nixpkgs-master-2023-07-18.url = "github:nixos/nixpkgs/08700de174bc6235043cb4263b643b721d936bdb";
     nixpkgs-master-2023-09-15.url = "github:nixos/nixpkgs/46688f8eb5cd6f1298d873d4d2b9cf245e09e88e";
     nixpkgs-master-2024-01-27.url = "github:nixos/nixpkgs/160b762eda6d139ac10ae081f8f78d640dd523eb";
+    nixpkgs-master-2024-10-19.url = "github:nixos/nixpkgs/67f803dcf82ebe958a53fcfa7c1edb276558c3f6";
     nixpkgs-unstable-2023-10-21.url = "github:nixos/nixpkgs/038b2922be3fc096e1d456f93f7d0f4090628729";
     nixpkgs-unstable-2024-02-20.url = "github:nixos/nixpkgs/b98a4e1746acceb92c509bc496ef3d0e5ad8d4aa";
     nixpkgs-unstable-2024-04-03.url = "github:nixos/nixpkgs/62e885a4013446453b10fd7780eba4337f6f42e0";
