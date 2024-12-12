567c302ef4464d0a7d264f3bc0f4f360e42ea591
Chris Martin <ch.martin@gmail.com>
2024-12-12 14:38:04 -0700

    move ghc 9.6.x to nixos 24.11 (#111)

--- main/ghc/checks.nix
+++ main/ghc/checks.nix
@@ -96,25 +96,25 @@ in
   ghc-9-6-3 = ghcCheck {
     packageName = "ghc-9-6-3";
     ghc = "9.6.3";
-    weeder = "2.8.0";
-    hls = "2.8.0.0";
+    weeder = "2.9.0";
+    hls = "2.9.0.0";
   };
   ghc-9-6-4 = ghcCheck {
     packageName = "ghc-9-6-4";
     ghc = "9.6.4";
-    weeder = "2.8.0";
-    hls = "2.8.0.0";
+    weeder = "2.9.0";
+    hls = "2.9.0.0";
   };
   ghc-9-6-5 = ghcCheck {
     packageName = "ghc-9-6-5";
     ghc = "9.6.5";
-    weeder = "2.8.0";
-    hls = "2.8.0.0";
+    weeder = "2.9.0";
+    hls = "2.9.0.0";
   };
   ghc-9-6-6 = ghcCheck {
     packageName = "ghc-9-6-6";
     ghc = "9.6.6";
-    weeder = "2.8.0";
+    weeder = "2.9.0";
     hls = "2.9.0.0";
   };
   ghc-9-8-1 = ghcCheck {
